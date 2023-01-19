3ba98cd8bd88167849739aff41cc1cf4548bca6b
RikuKetomaki <riku.ketomaki@gmail.com>
2023-01-19 12:47:08 +0200

    Added and done

---
+++ Harjoitukset 2/currencycalculator/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [eurot, setEurot] = useState(0)
+  const [punnat, setPunnat] = useState(0)
+
+  function laske (e) {
+    e.preventDefault()
+    setPunnat(eurot * 0.9)
+  }
+
+  // const laske= () => {
+  //   e.preventDefault()
+  //   setPunnat(eurot * 0.9)
+  // }
+
+  return (
+    <form onSubmit={laske}>
+      <h3>Valuuttalaskuri</h3>
+      <div>
+        <label>Eurot</label>
+        <input value={eurot} onChange={e => setEurot(e.target.value)} type="number"/>
+      </div>
+      <div>
+        <label>Punnat</label>
+        <output>{punnat}</output>
+      </div>
+      <button>Laske</button>
+    </form>
+  );
+}
+
+export default App;
